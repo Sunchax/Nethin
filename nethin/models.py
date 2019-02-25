21d738a894390cdbbcbba11e0e7097f33e780241
ched0021 <c15ced@cs.umu.se>
2019-02-25 14:15:41 +0100

    fixed bugg

--- nethin/models.py
+++ nethin/models.py
@@ -3367,7 +3367,7 @@ class GAN(BaseModel):
     def _gan_predict(self, x):
         
         model_G, model_D, model_GAN = self._model
-        return model_G.predic_on_batch(x)
+        return model_G.predict_on_batch(x)
 
     def save_models(self, G_path=None, D_path=None):
         
@@ -3896,7 +3896,7 @@ class WassersteinGAN(BaseModel):
     def _gan_predict(self, x):
         
         model_G, model_D, model_GAN = self._model
-        return model_G.predic_on_batch(x)
+        return model_G.predict_on_batch(x)
 
     def save_models(self, G_path=None, D_path=None):
         
